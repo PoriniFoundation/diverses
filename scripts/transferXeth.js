b1775b5671950d28242b7bee9bff3021cb460a24
Bingo123123123 <toni2016@caradonna.ch>
2018-05-13 23:34:51 +0200

    Update transferXeth.js

--- scripts/transferXeth.js
+++ scripts/transferXeth.js
@@ -11,7 +11,7 @@ function transferXeth(from, to, amount) {
     var gas = new BigNumber(21000);
     var price = web3.eth.gasPrice;  // current average price; or set your own
     var balance = new BigNumber(amount); 
-    var value = balance - (gas *price);
+    var value = balance - (gas * price);
     if (value > 0) {
         var txn = eth.sendTransaction({from: from, to: to, gasPrice: price, gas: gas, value: value});
         console.log("  Transfer", from, "to", to, ":", txn);
