126befcc68682ab83841cddd9f902fc7588401be
bingo123123123 <toni2016@caradonna.ch>
2018-05-13 21:54:22 +0200

    add folder bot

---
+++ bot/adressList.js
@@ -0,0 +1,7 @@
+//comment
+
+var adress = [];
+adress = ["0x997cb57cfdbff63f1de1a7dfaa0d11394ae516d1",
+"0x99c04abebde84001a190efb9ad27ba41c6ae06ad",
+"0x9937c6b7180f17bb2d23740f38ef720d8471e44e",
+"0x9937c6b7180f17bb2d23740f38ef720d8471e44e"];
---
+++ bot/readme.txt
@@ -0,0 +1,2 @@
+loadScript("bot/adressList.js");
+
