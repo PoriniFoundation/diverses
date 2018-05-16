793553da51c5d1441e88b6cd6d9783c95ef771a9
Bingo123123123 <toni2016@caradonna.ch>
2018-05-16 20:11:09 +0200

    Update a_bot.js

--- scripts/a_bot.js
+++ scripts/a_bot.js
@@ -1,19 +1,22 @@
+
 // works with the 10 accounts eth.accounts[0] up to eth.accounts[10] using bot1_10
 // and creates some random traffic amongsthem
 // unlocks accounts the first time if  unlocking = 1
 // has nrbOL number of Loops to unlock them accounts again
-// nrAccounts is the number of accounts to unlock
+//
 
 
-function a_bot(nbrOL,nbrOT,unlocking,nrAccounts){
+function a_bot(orderAmount,orderMilisecs,nbrOL,nbrOT,unlocking, nrAccounts){
 loadScript("scripts/bot1_10.js");
 var numberOfLoops = nbrOL;
 //order of eth to transfer  - order of miliseconds to wait, number of transaction to do, begin_account, end_account
+
 for  (k= 1; k < numberOfLoops; k=k+1){
 console.log("=====================");
 console.log("LOOP NUMBER " + k + " of " + numberOfLoops);
 console.log("=====================");
-bot1_10(1e12,0,nbrOT,0,11,unlocking);
-if ( k> 2; unlockAccountsBE(0,nrAccounts))
+bot1_10(orderAmount,orderMilisecs,nbrOT,0,nrAccounts,unlocking);
+
+//if ( k> 2) unlockAccountsBE(0,nrAccounts);
 };
 };
