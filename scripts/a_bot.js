e4060afadbf09f5c584e9c9116615400068c3ca5
Bingo123123123 <toni2016@caradonna.ch>
2018-05-16 16:28:02 +0200

    Update a_bot.js

--- scripts/a_bot.js
+++ scripts/a_bot.js
@@ -2,10 +2,10 @@
 // and creates some random traffic amongsthem
 // unlocks accounts the first time if  unlocking = 1
 // has nrbOL number of Loops to unlock them accounts again
-//
+// nrAccounts is the number of accounts to unlock
 
 
-function a_bot(nbrOL,nbrOT,unlocking){
+function a_bot(nbrOL,nbrOT,unlocking,nrAccounts){
 loadScript("scripts/bot1_10.js");
 var numberOfLoops = nbrOL;
 //order of eth to transfer  - order of miliseconds to wait, number of transaction to do, begin_account, end_account
@@ -14,6 +14,6 @@ console.log("=====================");
 console.log("LOOP NUMBER " + k + " of " + numberOfLoops);
 console.log("=====================");
 bot1_10(1e12,0,nbrOT,0,11,unlocking);
-unlockAccountsBE(0,11)
+if ( k> 2; unlockAccountsBE(0,nrAccounts))
 };
 };
